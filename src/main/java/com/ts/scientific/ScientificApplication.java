72d7a9339eafc1478aa373903337e4c78335ba63
ljw <ljw>
2020-02-14 21:05:40 +0800

    Initial commit

---
+++ src/main/java/com/ts/scientific/ScientificApplication.java
@@ -0,0 +1,13 @@
+package com.ts.scientific;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScientificApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScientificApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ts/scientific/ScientificApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ts.scientific;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScientificApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
