848b315a9d00c3cb9d572d88c4d667c1cc30b0f5
ljw <ljw>
2020-02-16 15:36:31 +0800

    代码生成

--- src/main/java/com/ts/scientific/service/UserService.java
+++ src/main/java/com/ts/scientific/service/UserService.java
@@ -3,6 +3,7 @@ package com.ts.scientific.service;
 import com.ts.scientific.entity.User;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.ts.scientific.mapper.UserMapper;
+import com.ts.scientific.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,4 +20,33 @@ public interface UserService extends IService<User> {
 
     Object login(String email, String password, HttpServletRequest request);
 
+    /**
+     * 查询所有用户显示列表
+     */
+    Object queryAllUser(UserVo userVo);
+
+
+    /**
+     * 添加用户
+     */
+    Object insertSelective(User record);
+
+
+
+    /**
+     * 修改用户
+     * @param record
+     * @return
+     */
+    Object updateByPrimaryKeySelective(User record);
+
+
+
+
+    /**
+     * 删除用户
+     * @param id
+     * @return
+     */
+    Object deleteByPrimaryKey(Integer id);
 }
