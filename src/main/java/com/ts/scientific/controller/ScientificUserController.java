9d85cba8583ee3ea7245d994d3a123f8f4f1ac19
ljw <ljw>
2020-02-16 12:41:55 +0800

    提交自动生成

---
+++ src/main/java/com/ts/scientific/controller/ScientificUserController.java
@@ -0,0 +1,12 @@
+package com.ts.scientific.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ */
+@RestController
+@RequestMapping("/ScientificUser")
+public class ScientificUserController {
+}
