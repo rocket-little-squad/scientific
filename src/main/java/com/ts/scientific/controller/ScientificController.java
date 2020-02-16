51f0cc8bc3784216a44190a53c5515758b6d0015
ljw <ljw>
2020-02-16 18:13:43 +0800

    科研信息

--- src/main/java/com/ts/scientific/controller/ScientificController.java
+++ src/main/java/com/ts/scientific/controller/ScientificController.java
@@ -1,8 +1,11 @@
 package com.ts.scientific.controller;
 
 
+import com.ts.scientific.dto.StatisticsDetailDto;
 import com.ts.scientific.service.StatisticsDetailService;
 import com.ts.scientific.service.impl.StatisticsDetailServiceImpl;
+import com.ts.scientific.util.PageUtil;
+import com.ts.scientific.util.RepResult;
 import com.ts.scientific.vo.StatisticsDetailVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  *
@@ -29,7 +34,15 @@ public class ScientificController {
          */
         @GetMapping("/getScientificDetail")
         public Object getScientificDetail(StatisticsDetailVO statisticsDetailVO){
-            return statisticsDetailServiceImpl.getStatistics(statisticsDetailVO);
+                if (statisticsDetailVO.getCurrentPage() != null && statisticsDetailVO.getPageSize() != null) {
+                        statisticsDetailVO.setCurrentPage((statisticsDetailVO.getCurrentPage() - 1) * statisticsDetailVO.getPageSize());
+                        statisticsDetailVO.setPageSize(statisticsDetailVO.getPageSize());
+                }
+                List<StatisticsDetailDto> statisticsDetailDtos = statisticsDetailServiceImpl.getStatistics(statisticsDetailVO);
+                statisticsDetailVO.setCurrentPage(null);
+                statisticsDetailVO.setPageSize(null);
+                int count = statisticsDetailServiceImpl.getStatistics(statisticsDetailVO).size();
+                return RepResult.repResult(0, "成功", statisticsDetailDtos, count);
         }
 
 }
